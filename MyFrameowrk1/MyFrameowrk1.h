8bcc48ae63b82561b7c6586845eab69aaba2513e
MIDIKinetics <moh@midikinetics.com>
2020-07-12 16:53:44 -0700

    Initial Commit

---
+++ MyFrameowrk1/MyFrameowrk1.h
@@ -0,0 +1,19 @@
+//
+//  MyFrameowrk1.h
+//  MyFrameowrk1
+//
+//  Created by Michael O. Hurwitz on 2020-07-12.
+//  Copyright © 2020 MIDI Kinetics. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyFrameowrk1.
+FOUNDATION_EXPORT double MyFrameowrk1VersionNumber;
+
+//! Project version string for MyFrameowrk1.
+FOUNDATION_EXPORT const unsigned char MyFrameowrk1VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyFrameowrk1/PublicHeader.h>
+
+
