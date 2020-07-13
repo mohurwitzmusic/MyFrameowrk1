f3caed16153aab9579913f6fdb1f9594f589f27e
MIDIKinetics <moh@midikinetics.com>
2020-07-12 17:17:05 -0700

    Added Foo Target

---
+++ FooTarget/FooTarget.h
@@ -0,0 +1,19 @@
+//
+//  FooTarget.h
+//  FooTarget
+//
+//  Created by Michael O. Hurwitz on 2020-07-12.
+//  Copyright © 2020 MIDI Kinetics. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FooTarget.
+FOUNDATION_EXPORT double FooTargetVersionNumber;
+
+//! Project version string for FooTarget.
+FOUNDATION_EXPORT const unsigned char FooTargetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FooTarget/PublicHeader.h>
+
+
