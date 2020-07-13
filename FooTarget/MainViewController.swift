cbdc68e7db293f51f2cafe75faa8516a6292e980
MIDIKinetics <moh@midikinetics.com>
2020-07-12 17:37:29 -0700

    Created MainViewController Added Foo Target as dependency

---
+++ FooTarget/MainViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MainViewController.swift
+//  MyFrameowrk1
+//
+//  Created by Michael O. Hurwitz on 2020-07-12.
+//  Copyright © 2020 MIDI Kinetics. All rights reserved.
+//
+
+import UIKit
+import FooTarget
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let _ = Foo()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
